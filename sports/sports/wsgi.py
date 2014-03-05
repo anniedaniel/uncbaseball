5358b77e4c019c7e71c0cbaebb07f89effe4a8e2
anniedaniel <anniedaniel001@gmail.com>
2014-03-05 13:11:03 -0500

    heroku settings added

--- sports/sports/wsgi.py
+++ sports/sports/wsgi.py
@@ -25,9 +25,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sports.settings")
 
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
-from dj_static import Cling
+# application = get_wsgi_application()
 
+from dj_static import Cling
 application = Cling(get_wsgi_application())
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
